bd99c8037b2410893b611a5860ae803f00a226cf
Andrew Collier <andrew@fathomdata.dev>
2025-09-01 15:48:17 +0100

    Bump version: 0.0.11 → 0.0.12

--- src/ibproxy/const.py
+++ src/ibproxy/const.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-VERSION = "0.0.11"
+VERSION = "0.0.12"
 
 # Use the loopback interface for security. If the proxy is hosted on EC2 this
 # will mean that requests from the internet will not reach the proxy.
